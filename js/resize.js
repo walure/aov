78d4176d78d199fa2b3703cd14fd7b3c16fea278
lu.wang <158322321@qq.com>
2018-07-08 18:57:23 +0800

    1

---
+++ js/resize.js
@@ -0,0 +1,16 @@
+(function (win, doc) {
+    if (!win.addEventListener) return;
+    var html = document.documentElement;
+    function setFont() {
+        var html = document.documentElement;
+        var k = 750;
+        html.style.fontSize = html.clientWidth / k * 100 + "px";
+    }
+    setFont();
+    setTimeout(function () {
+        setFont();
+    }, 300);
+    doc.addEventListener('DOMContentLoaded', setFont, false);
+    win.addEventListener('resize', setFont, false);
+    win.addEventListener('load', setFont, false);
+})(window, document);
